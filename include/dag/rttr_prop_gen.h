530b9589ec71775e2a91dc3e7cadf20ae8a5ca93
zz <xzrunner@gmail.com>
2020-05-02 08:34:50 +0800

    [CHANGED] support no PARM_FILEPATH

--- include/dag/rttr_prop_gen.h
+++ include/dag/rttr_prop_gen.h
@@ -1,7 +1,3 @@
-#ifndef PARM_FILEPATH
-#error "You must define PARM_FILEPATH macro before include this file"
-#endif
-
 #ifndef PARM_NODE_CLASS
 #error "You must define PARM_NODE_CLASS macro before include this file"
 #endif
@@ -23,7 +19,9 @@
 #define PARAM_INFO(id, type, name, member, default_val)                \
 .property(#name, &PARM_NODE_CLASS::Get##id, &PARM_NODE_CLASS::Set##id)
 #ifndef NO_PARM_FILEPATH
+#ifdef PARM_FILEPATH
 #include PARM_FILEPATH
+#endif // PARM_FILEPATH
 #endif // NO_PARM_FILEPATH
 #undef  PARAM_INFO
 
