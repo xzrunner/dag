5a81cc59375df077cc748b510dd8cc5f01ea67c0
zz <xzrunner@gmail.com>
2020-09-01 08:35:53 +0800

    rename

--- include/dag/node_parms_gen.h
+++ include/dag/node_parms_gen.h
@@ -2,22 +2,6 @@
 #error "You must define PARM_FILEPATH macro before include this file"
 #endif
 
-#define Bool    bool
-#define Int     int
-#define Int2    sm::ivec2
-#define Int3    sm::ivec3
-#define Int4    sm::ivec4
-#define Float   float
-#define Float2  sm::vec2
-#define Float3  sm::vec3
-#define Float4  sm::vec4
-#define Matrix2 sm::mat2
-#define Matrix3 sm::mat3
-#define Matrix4 sm::mat4
-#define String  std::string
-#define Array   std::vector
-#define Set     std::set
-
 // names
 public:
 static const constexpr char* const ParmNames[] = {
@@ -64,19 +48,3 @@ private:
     type member = type##default_val;
 #include PARM_FILEPATH
 #undef  PARAM_INFO
-
-#undef Bool
-#undef Int
-#undef Int2
-#undef Int3
-#undef Int4
-#undef Float
-#undef Float2
-#undef Float3
-#undef Float4
-#undef Matrix2
-#undef Matrix3
-#undef Matrix4
-#undef String
-#undef Array
-#undef Set
